490e57305e3d065d02ee49464e34f6080047e5ca
Jason Tu <title.kubo@gmail.com>
2017-05-30 10:34:02 -0400

    Added Sam's website
    Sam doesn't want website anymore.

---
+++ Websites/samborislow/js/main.js
@@ -0,0 +1,24 @@
+jQuery(function($) {
+
+	//Preloader
+	var preloader = $('.preloader');
+	$(window).load(function(){
+		preloader.remove();
+	});
+
+	//#main-slider
+	var slideHeight = $(window).height();
+	$('#home-slider .item').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('#home-slider .item').css('height',slideHeight);
+	});
+	
+	//Initiat WOW JS
+	new WOW().init();
+	//smoothScroll
+	smoothScroll.init();
+
+	
+});
+
